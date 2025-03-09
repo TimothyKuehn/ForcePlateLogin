7e020185258bd756610cd63efbbb57e46365eb1b
timothyAKuehn <timothyAKuehn@gmail.com>
2025-03-09 03:26:12 -0500

    added config file

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
-import MySQLdb.cursors
 import MySQLdb.cursors, re, hashlib
+import os
+
 app = Flask(__name__)
 
-# Change this to your secret key (it can be anything, it's for extra protection)
-app.secret_key = 'your secret key'
-
-# Enter your database connection details below
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root'
-app.config['MYSQL_DB'] = 'pythonlogin'
+# Load configuration from config.py if it exists, otherwise use default values
+if os.path.exists('config.py'):
+    app.config.from_pyfile('config.py')
+else:
+    app.secret_key = 'your secret key'
+    app.config['MYSQL_HOST'] = 'localhost'
+    app.config['MYSQL_USER'] = 'root'
+    app.config['MYSQL_PASSWORD'] = 'root'
+    app.config['MYSQL_DB'] = 'pythonlogin'
 
 # Intialize MySQL
 mysql = MySQL(app)
